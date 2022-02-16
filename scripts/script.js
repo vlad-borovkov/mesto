be578e2d188f24c99f84b4284ef05a453c4a5332
Vlad Borovkov <borovkov-vlad@ya.ru>
2022-02-17 02:35:05 +0700

    feat: сделать сценарий открытия, закрытия, сохранения текста в  popup

--- scripts/script.js
+++ scripts/script.js
@@ -11,10 +11,28 @@ let turnOffPopupVisability = function() {
 };
 
 popupOpenButton.addEventListener('click', turnOnPopupVisability);
-
 popupCloseButton.addEventListener('click', turnOffPopupVisability);
 
+function formSubmitHandler (evt) {
+    
+    let nameInput = document.querySelector('.popup__name');
+    let descriptionInput = document.querySelector('.popup__description');
+    
+    evt.preventDefault();
 
+    let nameOutput = document.getElementById('name');
+    let descriptionOutput = document.getElementById('description');
+    
+    let nameText = nameInput.value;
+    let descriptionText = descriptionInput.value;
+    
+    nameOutput.textContent = nameText; 
+    descriptionOutput.textContent = descriptionText;
+};
 
+let submitButton = document.querySelector('.popup__submit-button');
+submitButton.addEventListener('click', formSubmitHandler);
+submitButton.addEventListener('click', turnOffPopupVisability);
 
+ 
 
