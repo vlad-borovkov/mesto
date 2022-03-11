2c97e08122005679aff663e64f33b20e40e3c6d7
Vlad Borovkov <borovkov-vlad@ya.ru>
2022-03-11 14:11:16 +0700

    feat: добавить открыть\закрыть popup-place

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,13 @@
-let popupOpenButton = document.querySelector('.profile__info-edit-button');
-let popupCloseButton = document.querySelector('.popup__close-icone');
 let popup = document.querySelector('.popup');
+let popupAddPlace = document.querySelector('.popup-place');
+//место для popup с картинкой
+
+let popupOpenButton = document.querySelector('.profile__info-edit-button');
+let popupPlaceOpenButton = document.querySelector('.profile__add-button');
+let popupCloseButton = document.querySelector('.popup__close-icone');
+let popupAddPlaceCloseButton = document.querySelector('.popup-place__close-icone');
+
+
 
 let nameOutput = document.getElementById('name');
 let descriptionOutput = document.getElementById('description');
@@ -18,14 +25,23 @@ let turnOffPopupVisability = function() {
     popup.classList.remove('popup_on');
 };
 
+let turnOnPopupPlaceVisability = function() {
+    popupAddPlace.classList.add('popup-place_on');
+}
+
+let turnOffPopupPlaceVisability = function() {
+  popupAddPlace.classList.remove('popup-place_on');
+}
+
 function formSubmitHandler (evt) {
     evt.preventDefault();
     nameOutput.textContent = nameInput.value;
     descriptionOutput.textContent = descriptionInput.value;
-
     turnOffPopupVisability();
 };
 
+popupPlaceOpenButton.addEventListener('click', turnOnPopupPlaceVisability);
+popupAddPlaceCloseButton.addEventListener('click', turnOffPopupPlaceVisability);
 popupOpenButton.addEventListener('click', turnOnPopupVisability);
 popupCloseButton.addEventListener('click', turnOffPopupVisability);
 userInformation.addEventListener('submit', formSubmitHandler);
