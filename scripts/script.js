2ade265a88eb98a48560d4cfce160dc3835d54c5
Vlad Borovkov <borovkov-vlad@ya.ru>
2022-03-11 17:28:33 +0700

    feat: добавить скрипт клонирования template

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,57 @@
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+//массив фото-карточек
+
+
+const placeCardTemplate = document.querySelector('#placeCard').content;
+let placeCardContainer = document.querySelector('.photo-grid');
+
+const placeCardOnPage = placeCardTemplate.querySelector('.photo-grid__item').cloneNode(true);
+placeCardOnPage.querySelector('.photo-grid__card-image').src = 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg';
+placeCardOnPage.querySelector('.photo-grid__card-title').textContent = 'Черногория';
+
+placeCardContainer.append(placeCardOnPage);
+
 let popup = document.querySelector('.popup');
 let popupAddPlace = document.querySelector('.popup-place');
-//место для popup с картинкой
+//окна менюшек popup
 
 let popupOpenButton = document.querySelector('.profile__info-edit-button');
 let popupPlaceOpenButton = document.querySelector('.profile__add-button');
 let popupCloseButton = document.querySelector('.popup__close-icone');
 let popupAddPlaceCloseButton = document.querySelector('.popup-place__close-icone');
-
+//кнопки открытия-закрытия
 
 
 let nameOutput = document.getElementById('name');
 let descriptionOutput = document.getElementById('description');
-
 let nameInput = document.getElementById('userName');
 let descriptionInput = document.getElementById('userDescription');
+//поля для ввода текста в popup
 
 let turnOnPopupVisability = function() {
     popup.classList.add('popup_on');
